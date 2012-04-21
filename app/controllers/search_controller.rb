d6b77df3a0f30f1bd52ab041292c1bc70fb886c3
Shiva Nandan <seekshiva@gmail.com>
2012-04-22 05:24:26 +0530

    Modified the routing algorithm so that the search result page is now /search?q=.. instead of /search/search/?q=...
    Created a new control method search#home to render the home page, instead of search#index.
    
    search#index will now render the search result page instead of search#show

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
-  def index
+  def home
   end
   
-  def show
+  def index
     # @musics = Music.where("title = ?", params[:q])
     @query = params[:q]
     
@@ -15,7 +15,18 @@ class SearchController < ApplicationController
     cond[cond.length] = "album like ?" unless @album_checked.empty?
     cond[cond.length] = "artist like ?" unless @artist_checked.empty?
     
+    redirect_to :action => "index", :q => params[:q], :title => 1, :album => 1, :artist => 1 if cond.length == 0
+    
     @musics = Music.find(:all, :conditions => [ cond.join(" or ") ] + [ "%" + @query + "%" ] * cond.length , :order => "album ASC")
-
+    
+    @albums = {}
+    @musics.each do |music| 
+      sym = music.album.to_sym
+      if @albums[ sym ].nil?
+        @albums[sym] = []
+      end
+      @albums[sym][@albums[sym].length] = music
+    end
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ RemoveThis::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "search#index"
+  root :to => "search#home"
 
   # See how all your routes lay out with "rake routes"
 
